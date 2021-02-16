e72c4ebf73ae3ca42fe0ea6ed41719a28348339a
jolbek <jolbek.zade@protonmail.com>
2021-02-16 21:58:42 +0000

    created pages and classes related to SEAM-162

---
+++ src/test/java/com/seamlessly/stepdefinitions/UploadFile.java
@@ -0,0 +1,52 @@
+package com.seamlessly.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UploadFile {
+
+
+    @Given("login as user")
+    public void login_as_user() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user upload new file")
+    public void the_user_upload_new_file() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should locate uploaded file")
+    public void the_user_should_locate_uploaded_file() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user create new folder")
+    public void the_user_create_new_folder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should locate new folder")
+    public void the_user_should_locate_new_folder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user create new text file")
+    public void the_user_create_new_text_file() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should locate new text file")
+    public void the_user_should_locate_new_text_file() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
