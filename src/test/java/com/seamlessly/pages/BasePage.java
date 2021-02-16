f08fb6e394e0209e1dbad6476532d40211bc628a
aga <adamimdj@gmail.com>
2021-02-16 18:54:29 +0000

    main

---
+++ src/test/java/com/seamlessly/pages/BasePage.java
@@ -0,0 +1,14 @@
+package com.seamlessly.pages;
+
+import com.seamlessly.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+
+
+
+}
--- src/test/resources/features/Login.feature
+++
@@ -1,6 +0,0 @@
-Feature: login feature
-  @wip
-  Scenario: login with valid credentials
-  Given user is in login page
-    When user enters valid credentials
-    Then user should be able to login
