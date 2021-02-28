356a5dc02d491eeaec2fc441310cd01401a532a5
Rits-Github <ritesh_pandey@hotmail.com>
2021-02-28 07:19:05 +0000

    SettingsPage

--- src/test/java/com/seamlessly/pages/SettingsPage.java
+++ src/test/java/com/seamlessly/pages/SettingsPage.java
@@ -5,14 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class SettingsPage extends BasePage{
-    @FindBy(id ="displayname" )
+    @FindBy(id ="displayName" )
     public WebElement nameInput;
     @FindBy(id = "email")
     public WebElement emailInput;
-    @FindBy(id = "languageinput")
+    @FindBy(id = "language")
     public WebElement languageInput;
     @FindBy(id = "Phone")
-    public WebElement phoneumberInput;
+    public WebElement phoneNumberInput;
     @FindBy(id = "address")
     public WebElement addressInput;
     @FindBy(id = "website")
