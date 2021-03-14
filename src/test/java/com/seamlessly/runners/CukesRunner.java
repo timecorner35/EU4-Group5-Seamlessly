fdd9821b026a2c240193ec47c32870a9b704a8e7
Fatmakaraalan <fatmauk@hotmail.com>
2021-03-14 20:31:24 +0000

    SEAM-191 feature

--- src/test/java/com/seamlessly/runners/CukesRunner.java
+++ src/test/java/com/seamlessly/runners/CukesRunner.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/seamlessly/stepdefinitions",
-        dryRun = false,
-        tags = "@yus"
+        dryRun = true,
+        tags = "@SEAM-191"
 
 
 )
